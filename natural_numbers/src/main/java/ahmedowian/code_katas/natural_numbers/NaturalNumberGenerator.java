e6030b1537fee5cbf034af2fcdb19debceeaeecd
Ahmed Owian <ahmedowian@yahoo.com>
2016-04-21 11:45:00 -0400

    Natural Numbers Kata

---
+++ natural_numbers/src/main/java/ahmedowian/code_katas/natural_numbers/NaturalNumberGenerator.java
@@ -0,0 +1,25 @@
+package ahmedowian.code_katas.natural_numbers;
+
+/**
+ * If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. 
+ * The sum of these multiples is 23.
+ * Find the sum of all the multiples of 3 or 5 below 1000.
+ *
+ * @author Ahmed Owian
+ */
+public class NaturalNumberGenerator
+{
+    public static void main(String[] args) 
+    {
+        long sum = 0;
+        for (int i = 3; i < 1000; i++)
+        {
+            if (i % 3 == 0 || i % 5 == 0)
+            {
+                sum += i;
+            }
+        }
+        
+        System.out.println("Sum: " + sum);
+    }
+}
